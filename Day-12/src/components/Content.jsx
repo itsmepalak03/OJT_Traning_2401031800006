00a07083c6026c5c5df18364a98436fc3529d9f5
Palak Joshi <palakj0307@gmail.com>
2026-06-03 21:32:51 +0530

    Create Content.jsx

---
+++ Day-12/src/components/Content.jsx
@@ -0,0 +1,16 @@
+function Content() {
+  return (
+    <div className="content">
+      <h2>React Components Example</h2>
+
+      <p>
+        This is my first React application created using reusable
+        components.
+      </p>
+
+      <button>Learn More</button>
+    </div>
+  );
+}
+
+export default Content;
