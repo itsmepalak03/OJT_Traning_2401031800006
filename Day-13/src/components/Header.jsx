73ad8ee21dcffdb6f499e98a535add28be0ed7d3
Palak Joshi <palakj0307@gmail.com>
2026-06-03 21:34:58 +0530

    Create Header.jsx

---
+++ Day-13/src/components/Header.jsx
@@ -0,0 +1,25 @@
+const Header = () => {
+  return (
+    <div className="header">
+
+      <div>
+
+        <h2>
+          Welcome Back 👋
+        </h2>
+
+        <p>
+          Manage your antiques and reports.
+        </p>
+
+      </div>
+
+      <button className="btn btn-warning">
+        New Analysis
+      </button>
+
+    </div>
+  );
+};
+
+export default Header;
